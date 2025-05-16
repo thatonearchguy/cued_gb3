1547a29dab3c70658bf5964400cce2c2eb411d97
Yuvraj D <karandubey2911@gmail.com>
2025-05-16 09:42:03 +0100

    Added nixos config

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell rec {
+
+  buildInputs = [
+    pkgs.cmake
+    pkgs.nextpnr
+    pkgs.icestorm
+    pkgs.yosys
+    pkgs.iverilog
+    pkgs.libftdi
+    pkgs.pkg-config
+    pkgs.gnused
+    pkgs.arachne-pnr
+  ];
+
+  shellHook = ''
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH"
+  '';
+}
